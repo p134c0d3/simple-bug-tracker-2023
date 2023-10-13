32be759a8ec2e5ec62200f5c268938ded4d3effd
John P <70922864+p134c0d3@users.noreply.github.com>
2023-10-12 20:08:26 -0500

    Project Setup
    - installed and imported bootstrap 4.6, bootstrap js, and jquery
    - created components necessary
    - structured code as needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BugListComponent } from './bug-list/bug-list.component';
+import { BugComponent } from './bug/bug.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BugListComponent,
+    BugComponent
   ],
   imports: [
     BrowserModule
